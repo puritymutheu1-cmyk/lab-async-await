d52f670a790f7ad4dbd6556a1070a9c7a2cce8bd
puritymutheu <puritymutua6956@gmail.com>
2026-04-16 09:42:02 +0300

    final submission

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+function getPosts() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', false);
+  xhr.send();
+
+  const posts = JSON.parse(xhr.responseText);
+  const postList = document.getElementById('post-list');
+
+  posts.forEach(function(post) {
+    const h1 = document.createElement('h1');
+    h1.textContent = post.title;
+
+const p = document.createElement('p');
+p.textContent = post.body;
+
+postList.appendChild(h1);
+postList.appendChild(p);
+  });
+}
+
+getPosts();
+
+ 
+
